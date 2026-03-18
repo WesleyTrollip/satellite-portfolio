e4b928321f3da1ef73073d8463d7cf01f590e973
Wesley Trollip <wesleytrollip@gmail.com>
2026-03-18 20:44:48 +0100

    feat: add Swagger UI and update launchsettings

--- src/api/SatellitePortfolio.Api/Program.cs
+++ src/api/SatellitePortfolio.Api/Program.cs
@@ -9,7 +9,8 @@ using SatellitePortfolio.Domain;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddProblemDetails(options =>
 {
     options.CustomizeProblemDetails = context =>
@@ -69,7 +70,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseExceptionHandler();
@@ -82,6 +84,7 @@ using (var scope = app.Services.CreateScope())
 }
 
 app.MapControllers();
+app.MapGet("/", () => Results.Redirect("/swagger"));
 
 app.Run();
 
